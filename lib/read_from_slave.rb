bd6f0eb690fd3ec02ddca94c2fe7453e2e4d43bd
sdsykes <sdsykes@gmail.com>
2009-07-01 14:20:49 +0300

    First working version

--- lib/read_from_slave.rb
+++ lib/read_from_slave.rb
@@ -1,18 +1,17 @@
 module ReadFromSlave
   class << self
     def install!
-      @@slave_classes = {}
-      ActiveRecord::Base.send(:include, InstanceMethods)
-      ActiveRecord::Base.extend(UseSlaveDb)
+      ActiveRecord::Base.extend(SingletonMethods)
+      ActiveRecord::Base.class_eval do
+        class << self
+          alias_method_chain :find_by_sql, :read_from_slave
+          alias_method_chain :connection, :read_from_slave
+        end
+      end
     end
   end
   
-  module InstanceMethods
-    def self.included(base)
-      base.alias_method_chain :find_by_sql, :read_from_slave
-      base.alias_method_chain :connection, :read_from_slave
-    end
-
+  module SingletonMethods
     def find_by_sql_with_read_from_slave(sql)
       Thread.current[:read_from_slave] = true
       find_by_sql_without_read_from_slave(sql)
@@ -23,27 +22,26 @@ module ReadFromSlave
     def connection_with_read_from_slave
       normal_connection = connection_without_read_from_slave
       if Thread.current[:read_from_slave] && normal_connection.open_transactions == 0
-        @slave_connection || slave_connection_for(normal_connection)
+        @slave_connection || slave_connection
       else
         normal_connection
       end
     end
     
-    def slave_connection_for(normal_connection)
+    def slave_connection
       slave_class_name = "SlaveFor#{master_database_name}"
-      unless @@slave_connections[slave_class_name] 
+      @@slave_connections ||= {}
+      unless @@slave_connections[slave_class_name]
         eval "class #{slave_class_name} < ActiveRecord::Base; use_slave_db; end"
-        @slave_connections[slave_class_name] = slave_class.constantize.connection_without_read_from_slave
+        @@slave_connections[slave_class_name] = eval "#{slave_class_name}.connection_without_read_from_slave"
       end
-      @slave_connection = @slave_connections[slave_class_name]
+      @slave_connection = @@slave_connections[slave_class_name]
     end
 
     def master_database_name
       connection_without_read_from_slave.instance_variable_get(:@config)[:database]
     end
-  end
-  
-  module UseSlaveDb
+
     def use_slave_db
       conn_spec = configurations["slave_for_#{master_database_name}"]
       establish_connection(conn_spec) if conn_spec
