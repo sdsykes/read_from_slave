b6bef594b5ce1242371c1f43ec72879ec9a99f29
sdsykes <sdsykes@gmail.com>
2009-08-07 21:40:34 +0300

    Remove unused table from test schema

--- test/schema/schema.rb
+++ test/schema/schema.rb
@@ -1,9 +1,4 @@
 ActiveRecord::Schema.define do
-  create_table :entrants, :force => true do |t|
-    t.string  :name, :null => false
-    t.integer :course_id, :null => false
-  end
-  
   create_table :courses, :force => true do |t|
     t.column :name, :string, :null => false
   end
